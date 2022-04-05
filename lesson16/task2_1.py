50b683fde150d6e5065a159b1eb5d472d42b916a
basketbob <basketbob5@gmail.com>
2022-04-05 18:43:59 +0200

    lesson16 task2_1 tried new method extend for list

---
+++ lesson16/task2_1.py
@@ -0,0 +1,11 @@
+main = [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1]
+first_company = [0, 0, 0]
+second_company = [1, 0, 0, 1, 1]
+third_company = [1, 1, 1, 0, 1]
+
+main.extend(first_company)
+main.extend(second_company)
+main.extend(third_company)
+
+print(main)
+print(main.count(0))
