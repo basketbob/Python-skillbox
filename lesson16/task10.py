3434a383781f60e34295fed7605e137083dd9c14
basketbob <basketbob5@gmail.com>
2022-04-07 09:57:02 +0200

    lesson16 task10 optimized symmetric logic without function

--- lesson16/task10.py
+++ lesson16/task10.py
@@ -1,18 +1,10 @@
-def is_symmetric(list):
-    new_list = []
-    for v in list[::-1]:
-        new_list.append(v)
-
-    return new_list == list
-
-
-#list = [1,2,3,4,5]
-list = [1,2,1,2,2]
+list = [1,2,3,4,5] #[1,2,1,2,2]
 len_list = len(list)
 answer = []
 print('old list: ', list)
 for i in range(len_list):
-    if (is_symmetric(list[i:len_list])):
+    temp_list = list[i:len_list]
+    if (temp_list == temp_list[::-1]):
         answer = list[0:i]
         list.extend(answer[::-1])
         break
