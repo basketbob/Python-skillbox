c56858ab287394ac079c1ec16995a19eca2fd011
basketbob <basketbob5@gmail.com>
2022-04-07 09:39:36 +0200

    lesson16 task10.py symmetric list

---
+++ lesson16/task10.py
@@ -0,0 +1,21 @@
+def is_simmetria(list):
+    new_list = []
+    for v in list[::-1]:
+        new_list.append(v)
+
+    return new_list == list
+
+
+#list = [1,2,3,4,5]
+list = [1,2,1,2,2]
+len_list = len(list)
+answer = []
+print('old list: ', list)
+for i in range(len_list):
+    if (is_simmetria(list[i:len_list])):
+        answer = list[0:i]
+        list.extend(answer[::-1])
+        break
+
+print('answer:', answer)
+print('new list:', list)
