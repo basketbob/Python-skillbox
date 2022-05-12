154ffe2ce0e450182f372c8512975c74218e3bc5
basketbob <basketbob5@gmail.com>
2022-05-12 15:08:16 +0200

    use 2 classes with multiple instances

---
+++ lesson24/task2_2.py
@@ -0,0 +1,44 @@
+class Monitor:
+    name = 'Samsung'
+    matrix = 'VA'
+    res = 'WQHD'
+    freq = 60
+
+class Headphones:
+    name = ''
+    sensitivity = 108
+    micro = False
+
+monitor1 = Monitor()
+monitor1.name = 'Samsung'
+monitor1.matrix = 'VA'
+monitor1.res = 'WQHD'
+monitor1.freq = 60
+
+monitor2 = Monitor()
+monitor2.name = 'Samsung'
+monitor2.matrix = 'VA'
+monitor2.res = 'WQHD'
+monitor2.freq = 144
+
+monitor3 = Monitor()
+monitor3.name = 'Samsung'
+monitor3.matrix = 'VA'
+monitor3.res = 'WQHD'
+monitor3.freq = 70
+
+monitor4 = Monitor()
+monitor4.name = 'Samsung'
+monitor4.matrix = 'VA'
+monitor4.res = 'WQHD'
+monitor4.freq = 60
+
+headphones_name_1 = 'Sony'
+headphones_sensitivity_1 = 108
+headphones_micro_1 = False
+headphones_name_2 = 'Sony'
+headphones_sensitivity_2 = 108
+headphones_micro_2 = True
+headphones_name_3 = 'Sony'
+headphones_sensitivity_3 = 108
+headphones_micro_3 = True
